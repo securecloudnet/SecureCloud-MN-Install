a942644d315e38f833a7445a6591d38327ff3357
matricea <38187062+matricea@users.noreply.github.com>
2019-01-04 21:26:42 +0100

    new binaries added&some cosmetics

--- update.sh
+++ update.sh
@@ -1,12 +1,28 @@
 #/bin/bash
 
-cd ~
+#Setup Variables
+GREEN='\033[0;32m'
+YELLOW='\033[0;93m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e ${YELLOW}"Welcome to SecureCloud-2.2.0 Automated Update."${NC}
+echo "Please wait while updates are performed..."
+echo "Stopping the node"
+securecloud-cli stop
+sleep 10
+echo "Removing current binaries..."
 cd /usr/local/bin
-./securecloud-cli stop
 rm -rf securecloudd securecloud-cli securecloud-tx
-wget https://github.com/securecloudnet/SecureCloud/releases/download/2.1.0/securecloud-2.1.0-x86_64-linux-gnu.tar.gz
-tar -xzf securecloud-2.1.0-x86_64-linux-gnu.tar.gz
-rm -rf securecloud-2.1.0-x86_64-linux-gnu.tar.gz
-./securecloudd -daemon
-sleep 30
-./securecloud-cli getinfo
+echo "Downloading latest binaries"
+wget https://github.com/securecloudnet/SecureCloud/releases/download/2.2.0/SecureCloud-2.2.0-linux.tar.gz
+tar -xzf SecureCloud-2.2.0-linux.tar.gz
+sudo chmod 755 -R securecloud*
+rm -rf SecureCloud-2.2.0-linux.tar.gz
+cd ~
+echo "Startin/Syncing the node, please wait...";
+securecloudd -daemon
+until securecloud-cli mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+echo -e ${GREEN}"Your node is fully synced. Your masternode is running!"${NC}
+rm -rf /root/update.sh
+echo -e ${GREEN}"The END. You can close now the SSH terminal session"${NC};
