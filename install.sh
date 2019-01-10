969c29d6449f3e2988ad0f49cab8a276c425a114
matricea <38187062+matricea@users.noreply.github.com>
2019-01-10 19:10:03 +0100

    -e typo removed

--- install.sh
+++ install.sh
@@ -169,7 +169,7 @@ fi
 USERHOME=`eval echo "~$USER"`
 
 if [ -z "$ARGUMENTIP" ]; then
-read -e -p "Server IP Address: " -i $EXTERNALIP -e IP
+read -e -p "Server IP Address: " -i $EXTERNALIP IP
 fi
 
 if [ -z "$KEY" ]; then
